9dc3154b7ebdd7b6b1c09e73345f7346eaa24e77
ChristopherSTEEN <csteen59@gmail.com>
2017-05-12 15:35:22 +0200

    link the menu in the index page

--- index.php
+++ index.php
@@ -12,30 +12,7 @@
 		<script src="./js/script.js"></script>
 	</head>
 	<body>
-		<div id="menu">
-			<ul>
-				<li><a href="#">Gestions</a>
-					<ul>
-						<li><a href="#">Personnages</a></li>
-						<li><a href="#">Equipements</a></li>
-						<li><a href="#">Bijoux</a></li>
-						<li><a href="#">Résistances</a></li>
-						<li><a href="#">Fées</a></li>
-						<li><a href="#">Cartes SP</a></li>
-						<li><a href="#">Partenaires</a></li>
-						<li><a href="#">Cartes pour partenaires</a></li>
-						<li><a href="#">Equipements de partenaires</a></li>
-						<li><a href="#">Nosmates</a></li>
-					</ul>
-				</li>
-				<li><a href="#">Collections</a>
-					<ul>
-						<li><a href="#">Collections de SP</a></li>
-						<li><a href="#">Collections de cartes de partenaires</a></li>
-					</ul>
-				</li>
-			</ul>
-		</div>
+		<?php include_once ("menu.php"); ?>
 		<header><img id="index-logo" src="./img/logo.png"></img></header>
 		<div id="index-container">
 			<a class="index-server-link" href=""><img src="./img/sv1.png"></a>
