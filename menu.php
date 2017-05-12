ad2570f1bae49ba13a2ceac375870bc38608eaa4
ChristopherSTEEN <csteen59@gmail.com>
2017-05-12 15:35:02 +0200

    split the menu in a php file

---
+++ menu.php
@@ -0,0 +1,24 @@
+<div id="menu">
+	<ul>
+		<li><a href="#">Gestions</a>
+			<ul>
+				<li><a href="./ressources/characters.php">Personnages</a></li>
+				<li><a href="#">Equipements</a></li>
+				<li><a href="#">Bijoux</a></li>
+				<li><a href="#">Résistances</a></li>
+				<li><a href="#">Fées</a></li>
+				<li><a href="#">Cartes SP</a></li>
+				<li><a href="#">Partenaires</a></li>
+				<li><a href="#">Cartes pour partenaires</a></li>
+				<li><a href="#">Equipements de partenaires</a></li>
+				<li><a href="#">Nosmates</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Collections</a>
+			<ul>
+				<li><a href="#">Collections de SP</a></li>
+				<li><a href="#">Collections de cartes de partenaires</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
