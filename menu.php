c6bef9fb4c211aca053473b547aa67871501abf3
ChristopherSTEEN <csteen59@gmail.com>
2017-05-14 17:31:55 +0200

    change in menu file

--- menu.php
+++ menu.php
@@ -3,15 +3,15 @@
 		<li><a href="#">Gestions</a>
 			<ul>
 				<li><a href="./ressources/characters.php">Personnages</a></li>
-				<li><a href="#">Equipements</a></li>
-				<li><a href="#">Bijoux</a></li>
-				<li><a href="#">Résistances</a></li>
-				<li><a href="#">Fées</a></li>
-				<li><a href="#">Cartes SP</a></li>
-				<li><a href="#">Partenaires</a></li>
-				<li><a href="#">Cartes pour partenaires</a></li>
-				<li><a href="#">Equipements de partenaires</a></li>
-				<li><a href="#">Nosmates</a></li>
+				<li><a href="./ressources/equipements.php">Equipements</a></li>
+				<li><a href="./ressources/jewelries.php">Bijoux</a></li>
+				<li><a href="./ressources/resists.php">Résistances</a></li>
+				<li><a href="./ressources/fairies.php">Fées</a></li>
+				<li><a href="./ressources/cards.php">Cartes SP</a></li>
+				<li><a href="./ressources/partners.php">Partenaires</a></li>
+				<li><a href="./ressources/partnercards.php">Cartes pour partenaires</a></li>
+				<li><a href="./ressources/partnerequips.php">Equipements de partenaires</a></li>
+				<li><a href="./ressources/pets.php">Nosmates</a></li>
 			</ul>
 		</li>
 		<li><a href="#">Collections</a>
@@ -22,3 +22,4 @@
 		</li>
 	</ul>
 </div>
+<header><img id="index-logo" src="./img/logo.png" alt="Accueil"></img></header>
