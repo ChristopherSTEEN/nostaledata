8621edf6426001d0d6fe0f2fa8f836cfef795482
ChristopherSTEEN <csteen59@gmail.com>
2017-05-14 17:43:07 +0200

    add menu file for managements files

---
+++ ressources/menu.php
@@ -0,0 +1,25 @@
+<div id="menu">
+	<ul>
+		<li><a href="#">Gestions</a>
+			<ul>
+				<li><a href="./characters.php">Personnages</a></li>
+				<li><a href="./equipements.php">Equipements</a></li>
+				<li><a href="./jewelries.php">Bijoux</a></li>
+				<li><a href="./resists.php">Résistances</a></li>
+				<li><a href="./fairies.php">Fées</a></li>
+				<li><a href="./cards.php">Cartes SP</a></li>
+				<li><a href="./partners.php">Partenaires</a></li>
+				<li><a href="./partnercards.php">Cartes pour partenaires</a></li>
+				<li><a href="./partnerequips.php">Equipements de partenaires</a></li>
+				<li><a href="./pets.php">Nosmates</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Collections</a>
+			<ul>
+				<li><a href="#">Collections de SP</a></li>
+				<li><a href="#">Collections de cartes de partenaires</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
+<header><a href="../index.php"><img id="index-logo" src="../img/logo.png" alt="Accueil"></img></a></header>
