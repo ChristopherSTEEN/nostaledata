2d43f8c5aab2322ec7285c5eefd6926e1e8b8578
ChristopherSTEEN <csteen59@gmail.com>
2017-05-16 12:38:51 +0200

    add register ajax and display for equipements

--- ressources/equipements.php
+++ ressources/equipements.php
@@ -5,18 +5,53 @@
 <html>
 	<head>
 		<title>Gestions des équipements</title>
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="../css/default.css">
 		<link rel="icon" type="image/png" href="../img/icone.png">
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="../js/script.js"></script>
+		<script>
+			$(function(){
+				$("#equipements").on("submit", function(e){
+					e.preventDefault()
+					data = {
+						chara: $("#charaequip").val(),
+						img: $("#equippic").val(),
+						type: $("#equiptype").val(),
+						name: $("#equipname").val(),
+						level: $("#equiplv").val(),
+						rare: $("#equiprare").val(),
+						upgrade: $("#equipup").val(),
+						form: "equipements"
+					}
+					$.ajax({
+						method: "POST",
+						url: "../ajax/ajaxregister.php",
+						data : data,
+						success: function(success){
+							$("#globalerror").html(success);
+						}
+					})
+				})
+			})
+		</script>
 	</head>
 	<body>
 		<?php include_once ("./menu.php"); ?>
+		<div id="globalerror"></div>
 		<form action="./equipements.php" method="POST" id="equipements">
-			<div class="label">Personnage : </div><input type="text" id="charaequip" name="charaequip"><br/>
+			<div class="label">Personnage : </div>
+			<select name="charaequip" form="equipements" id="charaequip">
+				<?php
+					$charaequips = $PDO->query("SELECT * FROM characters ORDER BY ID");
+					foreach ($charaequips as $charow){
+						echo "<option value='" . $charow->ID . "'>" . $charow->pseudo . "</option>";
+					}
+				?>
+			</select><br/>
 			<div class="label">Image : </div><input type="file" id="equippic" name="equippic"><br/>
 			<div class="label">Type : </div>
-			<select name="equiptype" form="equipements">
+			<select name="equiptype" form="equipements" id="equiptype">
 				<option value="Arme principale">Arme principale</option>
 				<option value="Arme principale héroique">Arme principale héroique</option>
 				<option value="Arme secondaire">Arme secondaire</option>
@@ -27,7 +62,7 @@
 			<div class="label">Nom : </div><input type="text" id="equipname" name="equipname"><br/>
 			<div class="label">Niveau : </div><input type="number" step="1" min="1" max="99" id="equiplv" name="equiplv"><br/>
 			<div class="label">Rareté : </div>
-			<select name="equiprare" form="equipements">
+			<select name="equiprare" form="equipements" id="equiprare">
 				<option value="Endommagé">Endommagé</option>
 				<option value="Bas-Niveau">Bas-Niveau</option>
 				<option value="">Aucun</option>
@@ -57,6 +92,22 @@
 				</tr>
 			</thead>
 			<tbody>
+				<?php
+					$equipements = $PDO->query("SELECT e.*, c.pseudo FROM equipments e INNER JOIN characters c ON c.ID = e.character_id ORDER BY e.ID");
+					foreach ($equipements as $row){
+						echo "
+							<tr>
+								<td>" . $row->ID ."</td>
+								<td>" . $row->pseudo ."</td>
+								<td>" . $row->image_url ."</td>
+								<td>" . $row->equiptype ."</td>
+								<td>" . $row->name ."</td>
+								<td>" . $row->level ."</td>
+								<td>" . $row->rare ."</td>
+								<td>" . $row->upgrade ."</td>
+							</tr>";
+					}
+				?>
 			</tbody>
 		</table>
 	</body>
