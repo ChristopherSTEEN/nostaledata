a6b2212aaccc35c9affd4992aed40af02005c092
ChristopherSTEEN <csteen59@gmail.com>
2017-05-19 16:50:23 +0200

    add global display for resists

--- ressources/resists.php
+++ ressources/resists.php
@@ -9,14 +9,24 @@
 		<link rel="icon" type="image/png" href="../img/icone.png">
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="../js/script.js"></script>
+		<script src="../js/ajax.js"></script>
 	</head>
 	<body>
 		<?php include_once ("./menu.php"); ?>
+		<div id="globalerror"></div>
 		<form action="./resists.php" method="POST" id="resists">
-			<div class="label">Personnage : </div><input type="text" id="chararesist" name="chararesist"><br/>
+			<div class="label">Personnage : </div>
+			<select name="charares" form="resists" id="charares">
+				<?php
+					$charaequips = $PDO->query("SELECT * FROM characters ORDER BY ID");
+					foreach ($charaequips as $charow){
+						echo "<option value='" . $charow->ID . "'>" . $charow->pseudo . "</option>";
+					}
+				?>
+			</select><br/>
 			<div class="label">Image : </div><input type="file" id="resistpic" name="resistpic"><br/>
 			<div class="label">Type : </div>
-			<select name="resisttype" form="resists">
+			<select name="resisttype" form="resists" id="resisttype">
 				<option value="Gants">Gants</option>
 				<option value="Gants héroiques">Gants Héroiques</option>
 				<option value="Bottes">Bottes</option>
@@ -46,6 +56,24 @@
 				</tr>
 			</thead>
 			<tbody>
+				<?php
+					$equipements = $PDO->query("SELECT r.*, c.pseudo FROM resists r INNER JOIN characters c ON c.ID = r.character_id ORDER BY r.ID");
+					foreach ($equipements as $row){
+						echo "
+							<tr>
+								<td>" . $row->ID ."</td>
+								<td>" . $row->pseudo ."</td>
+								<td>" . $row->image_url ."</td>
+								<td>" . $row->restype ."</td>
+								<td>" . $row->name ."</td>
+								<td>" . $row->level ."</td>
+								<td>" . $row->fireres ." %</td>
+								<td>" . $row->waterres ." %</td>
+								<td>" . $row->lightres ." %</td>
+								<td>" . $row->darkres ." %</td>
+							</tr>";
+					}
+				?>
 			</tbody>
 		</table>
 	</body>
