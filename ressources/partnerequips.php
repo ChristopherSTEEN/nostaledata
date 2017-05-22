04ae6e2311c06775489aedd9339a44c322530583
ChristopherSTEEN <csteen59@gmail.com>
2017-05-22 16:04:01 +0200

    add display for partner equipments

--- ressources/partnerequips.php
+++ ressources/partnerequips.php
@@ -9,19 +9,29 @@
 		<link rel="icon" type="image/png" href="../img/icone.png">
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="../js/script.js"></script>
+		<script src="../js/ajax.js"></script>
 	</head>
 	<body>
 		<?php include_once ("./menu.php"); ?>
-		<form action="./partnerequips.php" method="POST" id="partnerequips">
-			<div class="label">Partenaire : </div><input type="text" id="partnerequip" name="partnerequip"><br/>
+		<div id="globalerror"></div>
+		<form action="./partnerequips.php" method="POST" id="partequips">
+			<div class="label">Partenaire : </div>
+			<select name="partequip" form="partequips" id="partequip">
+				<?php
+					$partners = $PDO->query("SELECT p.*, c.pseudo FROM partners p INNER JOIN characters c ON c.ID = p.character_id ORDER BY p.ID");
+					foreach ($partners as $parrow){
+						echo "<option value='" . $parrow->ID . "'>" . $parrow->name . " (" . $parrow->pseudo . ")</option>";
+					}
+				?>
+			</select><br/>
 			<div class="label">Type : </div>
-			<select name="pequiptype" form="partnerequips">
+			<select name="pequiptype" form="partequips" id="pequiptype">
 				<option value="Arme">Arme</option>
 				<option value="Armure">Armure</option>
 			</select><br/>
 			<div class="label">Niveau : </div><input type="number" step="1" min="1" max="99" id="pequiplv" name="pequiplv"><br/>
 			<div class="label">Rareté : </div>
-			<select name="pequiprare" form="partnerequips">
+			<select name="pequiprare" form="partequips" id="pequiprare">
 				<option value="Endommagé">Endommagé</option>
 				<option value="Bas-Niveau">Bas-Niveau</option>
 				<option value="">Aucun</option>
@@ -34,7 +44,7 @@
 				<option value="Légendaire">Légendaire</option>
 			</select><br/>
 			<div class="label">Amélioration : </div><input type="number" step="1" min="0" max="10" id="pequipup" name="pequipup"><br/>
-			<center><input type="submit" value="Enregistrer le partenaire" name="submit" id="submit"></center>
+			<center><input type="submit" value="Enregistrer l'équipement pour partenaire" name="submit" id="submit"></center>
 		</form>
 		<table id="pequiptable">
 			<thead>
@@ -48,6 +58,20 @@
 				</tr>
 			</thead>
 			<tbody>
+				<?php
+					$pequips = $PDO->query("SELECT p.*, pa.name AS partname FROM partnerequipments p INNER JOIN partners pa ON pa.ID = p.partner_id ORDER BY p.ID");
+					foreach ($pequips as $row){
+						echo "
+							<tr>
+								<td>" . $row->ID ."</td>
+								<td>" . $row->partname ."</td>
+								<td>" . $row->pequiptype ."</td>
+								<td>" . $row->level ."</td>
+								<td>" . $row->rare ."</td>
+								<td>" . $row->upgrade ."</td>
+							</tr>";
+					}
+				?>
 			</tbody>
 		</table>
 	</body>
