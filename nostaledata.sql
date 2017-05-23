98fd10a21846dc3834ca47532eb07f8e1b5c13a0
ChristopherSTEEN <csteen59@gmail.com>
2017-05-23 14:43:29 +0200

    update database file

--- nostaledata.sql
+++ nostaledata.sql
@@ -1,59 +1,52 @@
-CREATE DATABASE  IF NOT EXISTS `nostaledata` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `nostaledata`;
--- MySQL dump 10.13  Distrib 5.5.55, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: nostaledata
--- ------------------------------------------------------
--- Server version	5.5.55-0+deb8u1
+-- Host: localhost
+-- Generation Time: May 23, 2017 at 02:39 PM
+-- Server version: 5.5.55-0+deb8u1
+-- PHP Version: 5.6.30-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `nostaledata`
+--
+CREATE DATABASE IF NOT EXISTS `nostaledata` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `nostaledata`;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `cards`
 --
 
-DROP TABLE IF EXISTS `cards`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cards` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cards` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
+  `cardnumber` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `level` int(11) NOT NULL,
   `upgrade` int(11) DEFAULT NULL,
-  `reinforcement` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `reinforcement` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `cards`
---
-
-LOCK TABLES `cards` WRITE;
-/*!40000 ALTER TABLE `cards` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cards` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `characters`
 --
 
-DROP TABLE IF EXISTS `characters`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `characters` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `characters` (
+`ID` int(11) NOT NULL,
   `pseudo` varchar(20) NOT NULL,
   `image_url` text,
   `metier` varchar(45) NOT NULL,
@@ -65,223 +58,127 @@ CREATE TABLE `characters` (
   `herolv` int(11) NOT NULL,
   `heroprog` float NOT NULL,
   `gold` int(11) NOT NULL,
-  `reput` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
+  `reput` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `characters`
---
-
-LOCK TABLES `characters` WRITE;
-/*!40000 ALTER TABLE `characters` DISABLE KEYS */;
-/*!40000 ALTER TABLE `characters` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `equipments`
 --
 
-DROP TABLE IF EXISTS `equipments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `equipments` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `equipments` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `equiptype` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   `level` int(11) NOT NULL,
   `rare` varchar(45) NOT NULL,
-  `upgrade` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `upgrade` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `equipments`
---
-
-LOCK TABLES `equipments` WRITE;
-/*!40000 ALTER TABLE `equipments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `equipments` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `fairies`
 --
 
-DROP TABLE IF EXISTS `fairies`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `fairies` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `fairies` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `name` varchar(45) NOT NULL,
   `fireelem` int(11) DEFAULT NULL,
   `waterelem` int(11) DEFAULT NULL,
   `lightelem` int(11) DEFAULT NULL,
-  `darkelem` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `darkelem` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `fairies`
---
-
-LOCK TABLES `fairies` WRITE;
-/*!40000 ALTER TABLE `fairies` DISABLE KEYS */;
-/*!40000 ALTER TABLE `fairies` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `jewelries`
 --
 
-DROP TABLE IF EXISTS `jewelries`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `jewelries` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `jewelries` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `jeweltype` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `level` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
+  `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `jewelries`
---
-
-LOCK TABLES `jewelries` WRITE;
-/*!40000 ALTER TABLE `jewelries` DISABLE KEYS */;
-/*!40000 ALTER TABLE `jewelries` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `partnercards`
 --
 
-DROP TABLE IF EXISTS `partnercards`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `partnercards` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `partnercards` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `pcardtype` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `skillsrank` varchar(5) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `skillsrank` varchar(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `partnercards`
---
-
-LOCK TABLES `partnercards` WRITE;
-/*!40000 ALTER TABLE `partnercards` DISABLE KEYS */;
-/*!40000 ALTER TABLE `partnercards` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `partnerequipments`
 --
 
-DROP TABLE IF EXISTS `partnerequipments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `partnerequipments` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `partnerequipments` (
+`ID` int(11) NOT NULL,
   `partner_id` int(11) NOT NULL,
   `pequiptype` varchar(45) NOT NULL,
   `level` int(11) NOT NULL,
   `rare` int(11) DEFAULT NULL,
-  `upgrade` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `upgrade` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `partnerequipments`
---
-
-LOCK TABLES `partnerequipments` WRITE;
-/*!40000 ALTER TABLE `partnerequipments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `partnerequipments` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `partners`
 --
 
-DROP TABLE IF EXISTS `partners`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `partners` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `partners` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `parttype` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `level` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
+  `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `partners`
---
-
-LOCK TABLES `partners` WRITE;
-/*!40000 ALTER TABLE `partners` DISABLE KEYS */;
-/*!40000 ALTER TABLE `partners` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `pets`
 --
 
-DROP TABLE IF EXISTS `pets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pets` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `pets` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `name` varchar(45) NOT NULL,
   `level` int(11) NOT NULL,
   `atqlv` int(11) DEFAULT NULL,
-  `deflv` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `deflv` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `pets`
---
-
-LOCK TABLES `pets` WRITE;
-/*!40000 ALTER TABLE `pets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pets` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `resists`
 --
 
-DROP TABLE IF EXISTS `resists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `resists` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `resists` (
+`ID` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
   `image_url` text,
   `restype` varchar(45) NOT NULL,
@@ -290,27 +187,127 @@ CREATE TABLE `resists` (
   `fireres` int(11) DEFAULT NULL,
   `waterres` int(11) DEFAULT NULL,
   `lightres` int(11) DEFAULT NULL,
-  `darkres` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `darkres` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `resists`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `resists` WRITE;
-/*!40000 ALTER TABLE `resists` DISABLE KEYS */;
-/*!40000 ALTER TABLE `resists` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `cards`
+--
+ALTER TABLE `cards`
+ ADD PRIMARY KEY (`ID`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `characters`
+--
+ALTER TABLE `characters`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `equipments`
+--
+ALTER TABLE `equipments`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `fairies`
+--
+ALTER TABLE `fairies`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `jewelries`
+--
+ALTER TABLE `jewelries`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `partnercards`
+--
+ALTER TABLE `partnercards`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `partnerequipments`
+--
+ALTER TABLE `partnerequipments`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `partners`
+--
+ALTER TABLE `partners`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `pets`
+--
+ALTER TABLE `pets`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `resists`
+--
+ALTER TABLE `resists`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cards`
+--
+ALTER TABLE `cards`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `characters`
+--
+ALTER TABLE `characters`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `equipments`
+--
+ALTER TABLE `equipments`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fairies`
+--
+ALTER TABLE `fairies`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `jewelries`
+--
+ALTER TABLE `jewelries`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `partnercards`
+--
+ALTER TABLE `partnercards`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `partnerequipments`
+--
+ALTER TABLE `partnerequipments`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `partners`
+--
+ALTER TABLE `partners`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pets`
+--
+ALTER TABLE `pets`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `resists`
+--
+ALTER TABLE `resists`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-05-11 13:23:42
