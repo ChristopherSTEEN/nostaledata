430761fd58d4d0e26688c6ac09d050a806fa4236
ChristopherSTEEN <csteen59@gmail.com>
2017-06-22 14:41:43 +0200

    create partner card collection view

---
+++ ressources/cardcollection.php
@@ -0,0 +1,21 @@
+<?php
+	include_once ("../config.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Gestions des nosmates</title>
+		<link rel="stylesheet" href="../css/default.css">
+		<link rel="icon" type="image/png" href="../img/icone.png">
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="../js/script.js"></script>
+		<script src="../js/ajax.js"></script>
+	</head>
+	<body>
+		<?php include_once ("./menu.php"); ?>
+		<h1>Serveur 1</h1>
+		<h1>Serveur 2</h1>
+		<h1>Serveur 3</h1>
+		<h1>Serveur 4</h1>
+	</body>
+</html>
